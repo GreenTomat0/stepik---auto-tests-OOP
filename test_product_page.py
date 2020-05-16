438fde7d4028cd53ac533daeca3c42fca16111a5
taisia <taisia.miroshnichenko@arsenalmedia.ru>
2020-05-16 17:15:04 +0300

    проверяем работу страницы товара для страниц с разным url

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,20 @@ from .pages.product_page import ProductPage
 import pytest
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail), # этот тест падает. отметили его как xfail
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+	# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	# link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+	link = link
 	page = ProductPage(browser, link)
 	page.open() 
 	book_title = page.get_book_title() # получить название книги
@@ -15,6 +27,9 @@ def test_guest_can_add_product_to_basket(browser):
 	book_price_in_basket = page.book_price_in_basket() # получить цену книги, которая добавлена в корзину
 
 	# проверить, что название книги, которую добавляли в корзину, совпадает с названием книги, которая добавлена в корзину
-	assert book_title in book_title_in_basket, "No necessary book is added to the basket"
+	assert book_title == book_title_in_basket, "No necessary book is added to the basket"
 	# проверить, что цена книги, которую добавляли в корзину, совпадает с ценой книги, которая добавлена в корзину
-	assert book_price in book_price_in_basket, "prices don't match"
+	assert book_price == book_price_in_basket, "prices don't match"
+
+
+
