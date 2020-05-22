279c8dbc71a3c006c46cbe11098891c01ef84127
taisia <taisia.miroshnichenko@arsenalmedia.ru>
2020-05-22 14:46:32 +0300

    вернули обратно тест test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -43,6 +43,28 @@ class TestUserAddToBasketFromProductPage():
 		page.open() 
 		page.should_not_be_success_message()
 
+# незарегистрированный пользователь может добавить книги в корзину на страницах с промо-акцией
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail), # этот тест падает. отметили его как xfail
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+	link = link
+	page = ProductPage(browser, link)
+	page.open() 
+	book_title = page.get_book_title() # получить название книги
+	book_price = page.get_book_price() # получить цену книги
+	page.add_to_basket() # добавить в корзину
+	page.solve_quiz_and_get_code() # пройти задание в алерте
+	time.sleep(2)
+	book_title_in_basket = page.book_title_in_basket() # получить название книги, которая добавлена в корзину
+	book_price_in_basket = page.book_price_in_basket() # получить цену книги, которая добавлена в корзину
 
 # Добавляем товар в корзину и проверяем, что нет сообщения об успешном добавлении товара (это падающий тест - сообщение должно быть)
 @pytest.mark.xfail
